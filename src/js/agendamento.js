3ffa14193a57fc4c21c58fd49eb8b9a93c773d53
raul772 <raulvictor772@gmail.com>
2023-06-15 17:37:55 -0300

    Minor corrections

--- src/js/agendamento.js
+++ src/js/agendamento.js
@@ -1,25 +1,3 @@
-const logged = localStorage["logged"] === "true";
-const user_menu = document.querySelector("[user-dropdown]");
-const login_button = document.querySelector("[login-button]");
-const signup_button = document.querySelector("[signup-button]");
-const user =
-    localStorage["pettop-userinfo"] &&
-    JSON.parse(localStorage["pettop-userinfo"]);
-
-if (logged) {
-    user_menu.classList.remove("visually-hidden");
-    user_menu.querySelector("a").innerText = `${user.user}`;
-    login_button.classList.add("visually-hidden");
-    signup_button.classList.add("visually-hidden");
-} else {
-    window.location.href = "./login.html";
-}
-
-const logout = () => {
-    localStorage["logged"] = "false";
-    window.location.href = "./index.html";
-};
-
 const clickableDays = document.querySelectorAll(".clickable-day");
 clickableDays.forEach((day) => {
     day.addEventListener("click", () => {
