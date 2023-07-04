e184755f6089b1300aafb8ad4da572fbd7248f89
Raul772 <raulvictor772@gmail.com>
2023-07-04 17:38:16 -0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 #Dev Connection String
-# DATABASE_URL = "sqlite:///./petshop_database.db"
+DATABASE_URL = "sqlite:///./petshop_database.db"
 
 #Prod Connection String
-DATABASE_URL = "mysql+mysqldb://pettopapi:Raul88199262/@pettop.mysql.database.azure.com:3306/pettopbd"
+# DATABASE_URL = "mysql+mysqldb://pettopapi:Raul88199262/@pettop.mysql.database.azure.com:3306/pettopbd"
 
 engine = create_engine(DATABASE_URL)
 
