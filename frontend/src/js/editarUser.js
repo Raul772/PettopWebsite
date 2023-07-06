8f81f8a1c780d09d7fde3276d082d13c95a0e141
YuriSilv <yuridtna2002@hotmail.com>
2023-07-06 11:43:20 -0300

    editar user

---
+++ frontend/src/js/editarUser.js
@@ -0,0 +1,8 @@
+const userNameField = document.querySelector("#user-name");
+const userEmailField = document.querySelector("#user-email");
+const userAddressField = document.querySelector("#user-address");
+const userPhoneField = document.querySelector("#user-phone");
+
+const saveButton = document.querySelector("button[type='submit']");
+const cancelButton = document.querySelector("button[delete-service]");
+
